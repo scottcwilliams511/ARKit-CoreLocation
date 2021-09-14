e071cb852725a4f73f23f78d8345d4834f0bdf69
swilliams <swilliams@pnicorp.com>
2021-09-14 15:05:28 -0700

    Update SPM name.

--- Package.swift
+++ Package.swift
@@ -8,9 +8,9 @@ let package = Package(
     platforms: [ .iOS(.v9) ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "ARCL", targets: ["ARKit-CoreLocation"])
+        .library(name: "ARCL", targets: ["ARCL"])
     ],
     targets: [
-        .target(name: "ARKit-CoreLocation", dependencies: [])
+        .target(name: "ARCL", dependencies: [])
     ]
 )
